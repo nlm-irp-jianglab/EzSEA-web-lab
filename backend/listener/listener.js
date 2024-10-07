824adefcd4b44903cc716630f2a910bc9ce00c6c
root <root@inst-builder-debian-11-build-build-ly0tq.us-central1-a.c.gce-image-builder.internal>
2024-10-07 19:33:04 +0000

    ezsea ouput redirected

--- backend/listener/listener.js
+++ backend/listener/listener.js
@@ -16,17 +16,15 @@ app.post("/submit", (req, res) => {
     data = req.body;
     console.log(data);
     exec(`docker run --gpus all \
-          --mount type=bind,source=/home/jiangak_ncbi_nlm_nih_gov/EzSEA,target=/data \
+          --mount type=bind,source=/home/zhaoj16_ncbi_nlm_nih_gov/EzSEA/,target=/data \
           --mount type=bind,source=/home/jiangak_ncbi_nlm_nih_gov/database/,target=/database \
-          ezsea -i "${data.sequence}" --output "/home/zhaoj16_ncbi_nlm_nih_gov/outputs" -d "/database/GTDB" -n 1000 -f "${data.folding_program}" --treeprogram "${data.tree_program}" --asrprogram "${data.asr_program}"
+          ezsea -i "${data.sequence}" --output "/data/outputs" -d "/database/GTDB" -n 1000 -f "${data.folding_program}" --treeprogram "${data.tree_program}" --asrprogram "${data.asr_program}"
         `,    
     (err, stdout, stderr) => {
             if (err) {
                 console.error(err);
-                res.status(503).json({ error: "Error in running EzSEA command." });
-                return;
             } else {
-                res.status(200).json({ body: "Job submitted successfully" });
+                console.log("Job COMPLETED:", stdout);
             }
     });
     setTimeout(function () {
