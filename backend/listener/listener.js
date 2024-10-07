12c77e6d2c64e0b40a0ff6730f04920603ed9553
root <root@inst-builder-debian-11-build-build-ly0tq.us-central1-a.c.gce-image-builder.internal>
2024-10-07 14:33:36 +0000

    renaming tool image

--- backend/listener/listener.js
+++ backend/listener/listener.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { exec } = require('child_process');
-const allowOrigins = ['http://localhost:3000']
-
 
 var app = express();
 
@@ -17,17 +15,12 @@ app.post("/submit", (req, res) => {
     // Retrieve JSON from the POST body 
     data = req.body;
     console.log(data);
-    // exec(`EzSEA -i ${data.sequence} 
-    //             -o ${data.job_name}
-    //             -d /users/jiangak_ncbi_nlm_nih_gov/database/GTDB
-    //             -n 2000
-    //             -f ${data.folding_program}
-    //             -treeprogram ${data.tree_program}
-    //             -asrprogram ${data.asr_program}
-    //     `,
-    exec(`docker run reactvis-ezsea-tool ezsea
-        `,
-        (err, stdout, stderr) => {
+    exec(`docker run --gpus all \
+          --mount type=bind,source=/home/jiangak_ncbi_nlm_nih_gov/EzSEA,target=/data \
+          --mount type=bind,source=/home/jiangak_ncbi_nlm_nih_gov/database/,target=/database \
+          ezsea-tool "-i ${data.sequence} --output "/home/zhaoj16_ncbi_nlm_nih_gov/outputs" -d /database/GTDB -n 1000 -f ${data.folding_program} -treeprogram ${data.tree_program} -asrprogram ${data.asr_program}"
+        `,    
+    (err, stdout, stderr) => {
             if (err) {
                 console.error(err);
                 res.status(503).json({ error: "Error in running EzSEA command." });
@@ -38,11 +31,6 @@ app.post("/submit", (req, res) => {
             console.log(stdout);
         });
 });
-
-// app.get("/hello", (req, res) => {
-//     console.log("Recevied GET");
-//     res.send({ body: "Hello World!" });
-// });
 
 // Server listening on PORT 5000
 app.listen(5000, () => {
