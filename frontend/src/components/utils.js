981c5c239779711d2e49a1a7f19d485d2e0a2d18
littlelegz <zhaoj16@ncbi.nlm.nih.gov>
2024-10-15 09:34:10 -0400

    read/parse fa in utils

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -4,6 +4,45 @@ export const readFastaFromFile = async (path) => {
         .catch(error => console.error('Error fetching file:', error));
 }
 
+export const readFastaToDict = async (path) => {
+    try {
+        const fastaContent = await fetch(path).then(response => response.text());
+        const fastaDict = {};
+        let currentHeader = null;
+        let currentSequence = [];
+
+        const lines = fastaContent.split('\n');
+
+        lines.forEach(line => {
+            line = line.trim();
+
+            if (line.startsWith('>')) {
+                // If there's a current header, save the accumulated sequence before starting a new one
+                if (currentHeader) {
+                    fastaDict[currentHeader] = currentSequence.join('');
+                }
+
+                // Set the new header (excluding the '>')
+                currentHeader = line.slice(1).split(' ')[0]; // Take only the part before space
+                currentSequence = [];
+            } else if (currentHeader) {
+                // Accumulate sequence lines
+                currentSequence.push(line);
+            }
+        });
+
+        // Add the last sequence to the dictionary after looping through the lines
+        if (currentHeader) {
+            fastaDict[currentHeader] = currentSequence.join('');
+        }
+
+        return fastaDict;
+    } catch (error) {
+        console.error('Error fetching file:', error);
+        return null;
+    }
+};
+
 export const fastaToDict = async (fastaContent) => {
     const fastaDict = {};
     const lines = fastaContent.split('\n');
@@ -21,7 +60,7 @@ export const fastaToDict = async (fastaContent) => {
     });
     
     return fastaDict;
-}
+};
 
 // TODO Parse annotations, add colors
 export const addRingAnnotation = async (element, annotation) => { // attach rect to element, annotation is csv of annotations
